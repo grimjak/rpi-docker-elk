e41e065e493a8c015c39d6019db5f7cf8552c444
grimjak <graham@grimjak.org.uk>
2018-10-23 23:52:05 +0100

    fixed grok and added script

---
+++ evohome.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import sys
+from influxdb import InfluxDBClient
+from evohomeclient import EvohomeClient
+
+client = InfluxDBClient('192.168.1.204',8086,'root','root','telegraf')
+
+evoclient = EvohomeClient('info@grimjak.org.uk', 'Val1n0rho')
+
+jsonbody = []
+#time = int(time.time())
+for device in evoclient.temperatures():
+
+    if device["thermostat"]=="DOMESTIC_HOT_WATER" :
+      m = {
+        "measurement": "temperature",
+        "tags" :{
+          "room": "Hot water",
+          "sensor": "1"
+        },
+        "fields": {
+          "value": device["temp"]
+        },
+      }
+      jsonbody.append(m)
+    else:
+      m = {
+        "measurement": "temperature",
+        "tags" :{
+          "room": device["name"],
+          "sensor": "1"
+        },
+        "fields": {
+          "value": device["temp"]
+        }
+      }
+      jsonbody.append(m)
+      m = {
+        "measurement": "setpoint",
+        "tags" :{
+          "room": device["name"],
+          "sensor": "1"
+        },
+        "fields": {
+          "value": device["setpoint"]
+        }
+      }
+    jsonbody.append(m)
+
+client.write_points(jsonbody)
