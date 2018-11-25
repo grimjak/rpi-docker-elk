7b8efe8a51c794e20ac8d1c09bc9d05fe8f835cf
grimjak <graham@grimjak.org.uk>
2018-11-25 11:52:11 +0000

    added docker input to telegraf, added energy monitoring

--- telegraf/evohome.py
+++ telegraf/evohome.py
@@ -1,17 +1,10 @@
 #!/usr/local/bin/python
-import sys
-#from influxdb import InfluxDBClient
 from evohomeclient import EvohomeClient
-
-#client = InfluxDBClient('192.168.1.204',8086,'root','root','telegraf')
 
 evoclient = EvohomeClient('info@grimjak.org.uk', 'Val1n0rho')
 
-#time = int(time.time())
-
-
 for device in evoclient.temperatures():
-    if device["thermostat"]=="DOMESTIC_HOT_WATER" :
+    if device["thermostat"]=="DOMESTIC_HOT_WATER":
       print ("evohome,room=hotwater temperature="+str(device["temp"]))
     else:
-      print ("evohome,room="+device["name"].translate(str.maketrans({" ":"\ "}))+" temperature="+str(device["temp"])+",setpoint="+str(device["setpoint"]))
+      print ("evohome,room="+device["name"].translate(str.maketrans({" ": "\ "}))+" temperature="+str(device["temp"])+",setpoint="+str(device["setpoint"]))
---
+++ telegraf/octopus.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+import datetime
+import json
+
+import requests
+timefrom = (datetime.datetime.now()-datetime.timedelta(hours=24)).isoformat()
+contents = requests.get("https://api.octopus.energy/v1/electricity-meter-points/1012477457919/meters/17P1073309/consumption/?period_from="+str(timefrom), auth=('sk_live_qbE5sgjysVRpmEaPCY7LmfCP', '')).json()
+timestampstr = contents['results'][0]['interval_end']
+print(timestampstr)
+timestamp = datetime.datetime.strptime(timestampstr, "%Y-%m-%dT%H:%M:%SZ")
+consumption = contents['results'][0]['consumption']
+
+print('energy,type=electricity consumption='+str(consumption)+' '+str(timestamp.timestamp()))
