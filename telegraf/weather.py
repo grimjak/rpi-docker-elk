53ad3606a44d28d1cbd3265eca8d5407b9642c4a
grimjak <graham@grimjak.org.uk>
2018-11-27 21:34:44 +0000

    fixes to scripts

--- telegraf/weather.py
+++ telegraf/weather.py
@@ -2,7 +2,7 @@
 import datetime
 import requests
 
-contents = requests.get("http://api.openweathermap.org/data/2.5/weather?q=St%20Albans,GB&APPID=f44256870e8efe98c4f59aa15ee82b47")).json()
+contents = requests.get("http://api.openweathermap.org/data/2.5/weather?q=St%20Albans,GB&units=metric&APPID=f44256870e8efe98c4f59aa15ee82b47").json()
 temp = contents['main']['temp']
 
 print('weather,location=lon temperature='+str(temp))
